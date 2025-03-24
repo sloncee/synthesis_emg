69434d4db9b7ae9a16702ed461b09552afc0d648
00Fokus00 <iafilonov@mail.ru>
2025-03-24 15:40:37 +0300

    Interface for the effect of pizdez v2

--- Synthetic_EMG.py
+++ Synthetic_EMG.py
@@ -39,6 +39,8 @@ class App(ctk.CTk):
 
         self.protocol("WM_DELETE_WINDOW", self.on_close)
 
+        self.configure(fg_color="#FFFFFF")
+
         self.control_frame = ctk.CTkFrame(self)
         self.control_frame.grid(row=2, column=0, sticky="ns")
 
@@ -46,38 +48,49 @@ class App(ctk.CTk):
 
         self.duration_label = ctk.CTkLabel(self.control_frame, text="Протяженность:")
         self.duration_label.grid(row=0, column=0)
-        self.duration_spinbox = FloatSpinbox(self.control_frame, step_size=10, width=150)
+        self.duration_spinbox = FloatSpinbox(self.control_frame, step_size=100, width=150)
         self.duration_spinbox.set(10000)
         self.duration_spinbox.grid(row=1, column=0)
 
         self.start_time_label = ctk.CTkLabel(self.control_frame, text="Время старта:")
         self.start_time_label.grid(row=0, column=1)
-        self.start_time_spinbox = FloatSpinbox(self.control_frame, step_size=10, width=150)
+        self.start_time_spinbox = FloatSpinbox(self.control_frame, step_size=100, width=150)
         self.start_time_spinbox.set(2000)
         self.start_time_spinbox.grid(row=1, column=1)
 
         self.num_peaks_label = ctk.CTkLabel(self.control_frame, text="Кол-во пиков:")
         self.num_peaks_label.grid(row=0, column=2)
-        self.num_peaks_spinbox = FloatSpinbox(self.control_frame, step_size=10, width=150)
+        self.num_peaks_spinbox = FloatSpinbox(self.control_frame, step_size=100, width=150)
         self.num_peaks_spinbox.set(5000)
         self.num_peaks_spinbox.grid(row=1, column=2)
 
-        self.button = ctk.CTkButton(self, text="Показать график", command=self.synthesis_emg_plot)
-        self.button.grid(row=3, column=0)
+        self.button_frame = ctk.CTkFrame(self)
+        self.button_frame.grid(row=3, column=0, sticky="ns")
+
+        self.show_button = ctk.CTkButton(self.button_frame, text="Показать график", command=self.synthesis_emg_plot)
+        self.show_button.grid(row=0, column=0)
+
+        self.save_button = ctk.CTkButton(self.button_frame, text="Сохранить", command=self.save_emg)
+        self.save_button.grid(row=0, column=1)
+
+        # Относительный размер
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
     def create_empty_plot(self):
         self.fig, self.graph = plt.subplots(figsize=(12, 4))
         self.graph.set_title("Пустой график")
-        self.graph.set_xlabel("Ось X")
-        self.graph.set_ylabel("Ось Y")
+        self.graph.set_xlabel("X")
+        self.graph.set_ylabel("Y")
 
         self.canvas = FigureCanvasTkAgg(self.fig, master=self)
         self.canvas.draw()
-        self.canvas.get_tk_widget().grid(row=0, column=0)
+        self.canvas.get_tk_widget().grid(row=0, column=0, sticky="nsew")
 
         self.toolbar = CustomToolbar(self.canvas, self, pack_toolbar=False)
         self.toolbar.update()
         self.toolbar.grid(row=1, column=0)
+
 
     def synthesis_emg_plot(self):
         duration = self.duration_spinbox.get()
@@ -93,7 +106,7 @@ class App(ctk.CTk):
             return
 
         ticks = np.arange(0, duration, 1)
-        signal = np.zeros_like(ticks, dtype=float)
+        self.signal = np.zeros_like(ticks, dtype=float)
 
         for _ in range(num_peaks):
             peak_position = np.random.randint(start_time, duration)
@@ -101,20 +114,27 @@ class App(ctk.CTk):
             sigma = np.random.uniform(5, 50)
 
             time_range = np.arange(- 3 * sigma, 3 * sigma, 1)
-            if peak_position + len(time_range) < len(signal):
-                signal[peak_position:peak_position + len(time_range)] += gaussian_derivative(time_range, sigma,
+            if peak_position + len(time_range) < len(self.signal):
+                self.signal[peak_position:peak_position + len(time_range)] += gaussian_derivative(time_range, sigma,
                                                                                              amplitude)
 
-        signal += noise(signal)
+        self.signal += noise(self.signal)
 
         self.graph.clear()
-        self.graph.plot(signal, label="Synthetic EMG", linewidth=1)
+        self.graph.plot(self.signal, label="Synthetic EMG", linewidth=1)
         self.graph.set_title("Синтез ЭМГ")
         self.graph.set_xlabel("Time (ms)")
         self.graph.set_ylabel("Amplitude")
         self.graph.legend()
         self.graph.grid()
         self.canvas.draw()
+
+    def save_emg(self):
+        if self.signal is not None:
+            np.savetxt("synthetic_emg.txt", self.signal, fmt="%.6f")
+            messagebox.showinfo("Успех", "Данные сохранены в synthetic_emg.txt")
+        else:
+            messagebox.showerror("Ошибка", "Нет данных для сохранения")
 
     def on_close(self):
         plt.close(self.fig)
